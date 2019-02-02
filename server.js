20e891cb4d7f3140b94838b3cf7a22dd22347c36
imcdonald1 <imcdonald1@mail.csuchico.edu>
2019-02-02 10:26:14 -0800

    required models

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-let models = require(__dirname + 'models/models.js');
+let models = require(__dirname + '/models/models.js');
 const app =  express();
 const PORT = process.env.PORT || 3000;
 
