3e2c79971fcc6261566985b0ac6e91067d5ac2f1
imcdonald1 <imcdonald1@mail.csuchico.edu>
2019-02-02 11:58:15 -0800

    updated models

--- models/models.js
+++ models/models.js
@@ -4,14 +4,16 @@ let itemSchema = new mongoose.Schema({
 	name: String,
 	value: String,
 	alias: [String],
-	used: {type: Boolean, default: false}
+	used: {type: Boolean, default: false},
+	userId: String
 });
 
 let itemCollectionSchema = new mongoose.Schema({
 	name: String,
-	items: [itemSchema],
-	alias: [String]
-})
+	items: [String],
+	alias: [String],
+	userId: String
+});
 
-let Item = module.exports = mongoose.model('Item', itemSchema);
+let item = module.exports = mongoose.model('item', itemSchema);
 let itemCollection = module.exports = mongoose.model('itemCollection', itemCollectionSchema);
